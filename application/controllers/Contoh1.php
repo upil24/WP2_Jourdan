dc9da70ad0233e0bbf8a7a6e5c7da1878bbc52e0
upil24 <bikinnamaajaribet@gmail.com>
2021-09-23 01:48:54 +0700

    latihan pertemuan 2 clearrrr

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Jourdan
+        Saya tinggal didaerah Bekasi
+        Olahraga yang saya sukai adalah bulu tangkis";
+    }
+}
